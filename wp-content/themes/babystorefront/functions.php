6888f1d098ca13593095a1141dba85f554816355
cappeloow <cappe1996@hotmail.com>
2023-04-19 22:51:46 +0200

    removed google listing

--- wp-content/themes/babystorefront/functions.php
+++ wp-content/themes/babystorefront/functions.php
@@ -9,6 +9,21 @@ function isDownloadable()
 }
 add_action('woocommerce_before_single_product_summary', 'isDownloadable', 10);
 
+function my_custom_product_filters()
+{
+    $args = array(
+        'taxonomy' => 'product_cat',
+        'show_option_all' => 'Kategorier',
+        'orderby' => 'name',
+        'class' => 'cat_filter',
+        'name' => 'product_cat',
+        'value_field' => 'slug',
+        'selected' => isset($_GET['product_cat']) ? $_GET['product_cat'] : '',
+    );
+    wp_dropdown_categories($args);
+}
+add_action('woocommerce_after_shop_loop', 'my_custom_product_filters', 9);
+add_action('woocommerce_before_shop_loop', 'my_custom_product_filters', 9);
 
 
 
